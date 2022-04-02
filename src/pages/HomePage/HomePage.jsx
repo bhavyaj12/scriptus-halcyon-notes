c92e3f7de2ac48101119e43c6aab09c4a3490751
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-04-02 23:17:45 +0530

    style: format code

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,25 +2,36 @@ import "./home-page.css";
 import { LandingImg } from "../../assets";
 
 const HomePage = () => {
-    return (
-        <div className="container-landing ">
-            <img src={LandingImg} alt="girl notes" className="img-landing" />
+  return (
+    <main className="page-container">
+      <div className="container-landing ">
+        <img src={LandingImg} alt="girl notes" className="img-landing" />
 
-            <div className="container-landing-text">
-                <div className="heading-main">
-                    <h1 className="h1">Meet your <span style={{color:"var(--cta-notes)"}}>Modern Note Taking App</span></h1>
-                </div>
-                <div className="subheading-main">
-                    Scribble down all your unique and amazing ideas, give yourself a productivity boost by managing your task list and more, using Scriptus.
-                </div>
-                <div className="button-landing">
-                    <a href="">
-                        <button className="btn-notes-home btn-primary">Start Taking Notes</button>
-                    </a>
-                </div>
-            </div>
+        <div className="container-landing-text">
+          <div className="heading-main">
+            <h1 className="h1">
+              Meet your{" "}
+              <span style={{ color: "var(--cta-notes)" }}>
+                Modern Note Taking App
+              </span>
+            </h1>
+          </div>
+          <div className="subheading-main">
+            Scribble down all your unique and amazing ideas, give yourself a
+            productivity boost by managing your task list and more, using
+            Scriptus.
+          </div>
+          <div className="button-landing">
+            <a href="">
+              <button className="btn-notes-home btn-primary">
+                Start Taking Notes
+              </button>
+            </a>
+          </div>
         </div>
-    )
+      </div>
+    </main>
+  );
 };
 
 export { HomePage };

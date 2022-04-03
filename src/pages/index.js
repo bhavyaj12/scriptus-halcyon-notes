40c386b454902a80fa81d600bcfd72fc514763b6
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-04-03 22:50:00 +0530

    feat: added empty notes page with sidebar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { HomePage } from "./HomePage/HomePage";
 export { LoginPage } from "./Authentication/LoginPage";
 export { SignupPage } from "./Authentication/SignupPage";
+export { NotesPage } from "./NotesPage/NotesPage";

1943889ef8f8af35dc0e72519d1208ca3437865f
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-04-03 22:53:39 +0530

    chore: class to className, for to htmlFor

--- src/pages/Authentication/SignupPage.jsx
+++ src/pages/Authentication/SignupPage.jsx
@@ -4,35 +4,35 @@ import "./signup.css";
 
 const SignupPage = () => {
     return(
-        <main class="form-container">
-        <h4 class="h4">SIGN UP</h4>
-        <p class="my-5">Signing Up will give you access to special discounts right in your email inbox!</p>
-        <form action="" class="flex-col">
-            <div class="form-row flex">
-                <label for="firstname" class="input-label input-required ">First Name</label>
-                <input id="firstname" type="text" class="input-field my-5" placeholder="Enter your first name here"
+        <main className="form-container">
+        <h4 className="h4">SIGN UP</h4>
+        <p className="my-5">Signing Up will give you access to special discounts right in your email inbox!</p>
+        <form action="" className="flex-col">
+            <div className="form-row flex">
+                <label htmlFor="firstname" className="input-label input-required ">First Name</label>
+                <input id="firstname" type="text" className="input-field my-5" placeholder="Enter your first name here"
                     required />
             </div>
-            <div class="form-row flex">
-                <label for="lastname" class="input-label input-required ">Last Name</label>
-                <input id="lastname" type="text" class="input-field my-5" placeholder="Enter your last name here"
+            <div className="form-row flex">
+                <label htmlFor="lastname" className="input-label input-required ">Last Name</label>
+                <input id="lastname" type="text" className="input-field my-5" placeholder="Enter your last name here"
                     required />
             </div>
-            <div class="form-row flex">
-                <label for="email-id" class="input-label input-required ">Email address</label>
-                <input id="email-id" type="email" class="input-field my-5" placeholder="Enter your email here" required />
+            <div className="form-row flex">
+                <label htmlFor="email-id" className="input-label input-required ">Email address</label>
+                <input id="email-id" type="email" className="input-field my-5" placeholder="Enter your email here" required />
             </div>
-            <div class="form-row flex">
-                <label class="input-label input-required ">Password</label>
-                <div class="my-5 hide-pswrd">
-                    <input type="password" class="input-field" placeholder="Enter password" required maxlength="20"
-                        minlength="6" />
-                    <span><i class="fa fa-eye-slash"></i></span>
+            <div className="form-row flex">
+                <label className="input-label input-required ">Password</label>
+                <div className="my-5 hide-pswrd">
+                    <input type="password" className="input-field" placeholder="Enter password" required maxLength="20"
+                        minLength="6" />
+                    <span><i className="fa fa-eye-slash"></i></span>
                 </div>
             </div>
-            <button class="button button-primary btn-solid login-btn">Sign Up</button>
-            <div class="signup-msg my-5">Already a user?{" "}
-                <Link to="/login" class="button button-primary button-link">
+            <button className="button button-primary btn-solid login-btn">Sign Up</button>
+            <div className="signup-msg my-5">Already a user?{" "}
+                <Link to="/login" className="button button-primary button-link">
                     Login here
                 </Link>.
             </div>

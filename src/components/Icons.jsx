6ed40075022bbcc9fee9520fbfa5c93d925f3fed
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-04-05 01:43:41 +0530

    feat: added icons and index

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -2,5 +2,9 @@ import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import LabelRoundedIcon from '@mui/icons-material/LabelRounded';
 import ArchiveRoundedIcon from '@mui/icons-material/ArchiveRounded';
 import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
+import PushPinRoundedIcon from "@mui/icons-material/PushPinRounded";
+import PaletteRoundedIcon from "@mui/icons-material/PaletteRounded";
+import PushPinOutlinedIcon from "@mui/icons-material/PushPinOutlined";
+import EditRoundedIcon from '@mui/icons-material/EditRounded';
 
-export { HomeRoundedIcon, LabelRoundedIcon, ArchiveRoundedIcon, DeleteRoundedIcon };
+export { HomeRoundedIcon, LabelRoundedIcon, ArchiveRoundedIcon, DeleteRoundedIcon, PushPinRoundedIcon, PaletteRoundedIcon, PushPinOutlinedIcon, EditRoundedIcon };
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 export { NavTop } from "./NavTop/NavTop";
 export { Footer } from "./Footer/Footer";
 export { NavSide } from "./NavSide/NavSide";
-export { HomeRoundedIcon, LabelRoundedIcon, ArchiveRoundedIcon, DeleteRoundedIcon } from "./Icons";
+export { NoteCard } from "./NoteItems/NoteCard";
+export { AddNote } from "./NoteItems/AddNote";
+export { HomeRoundedIcon, LabelRoundedIcon, ArchiveRoundedIcon, DeleteRoundedIcon, PushPinRoundedIcon, PaletteRoundedIcon, PushPinOutlinedIcon, EditRoundedIcon } from "./Icons";

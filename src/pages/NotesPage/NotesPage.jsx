808964363372213690a4afa6513891e535e636e0
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-04-05 01:44:44 +0530

    feat: map over added cards on homepage

--- src/pages/NotesPage/NotesPage.jsx
+++ src/pages/NotesPage/NotesPage.jsx
@@ -1,14 +1,27 @@
-import { NavSide } from "../../components";
+import { useNote } from "../../contexts";
+import { NavSide, NoteCard, AddNote } from "../../components";
 import "./notes-page.css";
 
 const NotesPage = () => {
+  const {
+    noteState: { notes },
+  } = useNote();
   return (
     <main className="grid-col-2">
       <div className="grid-col-item">
         <NavSide />
       </div>
-      <div className="grid-col-item">
-        <h1>Here you take the notes</h1>
+      <div className="grid-col-item flex-col">
+        <section className="notes-editor-container">
+          <AddNote />
+          <section className="notes-display">
+            {notes.length > 0 ? (
+              notes.map((item) => <NoteCard note={item} key={item._id} />)
+            ) : (
+              <h3 className="h3">Saved Notes appear here</h3>
+            )}
+          </section>
+        </section>
       </div>
     </main>
   );

5094d61b5b8a81ae9e164e5ca8d792dd81f227e6
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-04-07 15:19:37 +0530

    fix: added 0 to date less than 10

--- src/components/NoteItems/AddNote.jsx
+++ src/components/NoteItems/AddNote.jsx
@@ -22,7 +22,7 @@ const AddNote = () => {
 
   const getDate = () => {
     const date = new Date();
-    const day = date.getDate();
+    const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
     const month = date.getMonth() < 9 ? `0${date.getMonth() + 1}` : date.getMonth()+1;
     const year = date.getFullYear();
     const hour = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();

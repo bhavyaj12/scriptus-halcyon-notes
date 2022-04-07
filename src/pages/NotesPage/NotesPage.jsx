88bde60f5bedc97a676e9b6515b0dd9b4bbee973
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-04-07 17:50:25 +0530

    feat: added filter in notes page with style

--- src/pages/NotesPage/NotesPage.jsx
+++ src/pages/NotesPage/NotesPage.jsx
@@ -1,11 +1,15 @@
 import { useNote } from "../../contexts";
+import { sortDateTime } from "../../utilities";
 import { NavSide, NoteCard, AddNote, Modal } from "../../components";
 import "./notes-page.css";
 
 const NotesPage = () => {
   const {
-    noteState: { notes, showModal },
+    noteState: { notes, showModal, sortByDateTime },
+    noteDispatch,
   } = useNote();
+
+  const sortedNotes = sortDateTime(notes, sortByDateTime);
   return (
     <main className="grid-col-2">
       <div className="grid-col-item">
@@ -13,10 +17,55 @@ const NotesPage = () => {
       </div>
       <div className="grid-col-item flex-col">
         <section className="notes-editor-container">
+          <div className="filter-container flex-row">
+            <h5 className="h5">Sort By:</h5>
+            <div className="radio-container">
+              <label htmlFor="latest-first">Latest First</label>
+              <input
+                type="radio"
+                id="latest-first"
+                name="notes-sort-date"
+                className="mx-3"
+                value="new-to-old"
+                checked={sortByDateTime === "NEWEST_FIRST"}
+                onChange={(e) =>
+                  noteDispatch({
+                    type: "SORT_BY_CREATED_ON",
+                    payload: e.target.value,
+                  })
+                }
+              />
+            </div>
+            <div className="radio-container">
+              <label htmlFor="oldest-first">Oldest First</label>
+              <input
+                type="radio"
+                id="oldest-first"
+                name="notes-sort-date"
+                className="mx-3 text-normal"
+                value="old-to-new"
+                checked={sortByDateTime === "OLDEST_FIRST"}
+                onChange={(e) =>
+                  noteDispatch({
+                    type: "SORT_BY_CREATED_ON",
+                    payload: e.target.value,
+                  })
+                }
+              />
+            </div>
+            <p
+              className="button button-link"
+              onClick={() => {
+                noteDispatch({ type: "SORT_BY_CREATED_ON", payload: "" });
+              }}
+            >
+              Clear
+            </p>
+          </div>
           <AddNote />
           <section className="notes-display">
-            {notes.length > 0 ? (
-              notes.map((item) => <NoteCard note={item} key={item._id} />)
+            {sortedNotes.length > 0 ? (
+              sortedNotes.map((item) => <NoteCard note={item} key={item._id} />)
             ) : (
               <h3 className="h3">Saved Notes appear here</h3>
             )}
